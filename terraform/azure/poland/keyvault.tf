44b54668a01e28b1bae0a3a1d089ff07ff07e316
Catherine Vee <catherine.vee@outlook.com>
2025-04-10 17:47:07 -0700

    update keyvault.tf

--- terraform/azure/poland/keyvault.tf
+++
@@ -1,9 +0,0 @@
-module "avm-res-keyvault-vault" {
-  source  = "Azure/avm-res-keyvault-vault/azurerm"
-  version = "0.10.0"
-  location = var.defaultlocation
-  name = "${var.defaultlocation}-keyvault"
-  resource_group_name = var.defaultrg
-  tenant_id = var.AZURE_TENANT_ID
-
-}
