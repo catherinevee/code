bb5f73f1f9eee5b0c24e7fc4b8fe92905afadc80
Catherine Vee <catherine.vee@outlook.com>
2025-04-10 17:08:50 -0700

    create main_variables.tf

---
+++ terraform/main_variables.tf
@@ -0,0 +1,11 @@
+variable "AWS_ACCESS_KEY_ID" {}
+
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+variable "AZURE_CLIENT_ID" {}
+
+variable "AZURE_CLIENT_SECRET" {}
+
+variable "ARM_SUBSCRIPTION_ID" {}
+
+variable "AZURE_TENANT_ID" {}
