9824ae2443ba6336f6c685e6b7499b1bdb4d6f81
Catherine Vee <catherine.vee@outlook.com>
2025-04-11 11:05:51 -0700

    modify git sync yml file

--- commit.sh
+++ commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AZ_USERNAME=${{secrets.AZ_USERNAME}}
-AZ_PAT=${{secrets.AZURE_CLIENT_SECRET}}
+AZ_AZURE_CLIENT_SECRET=${{secrets.AZURE_CLIENT_SECRET}}
 AZ_ORG=${{secrets.AZ_ORG}}
 AZ_USERNAME=${{secrets.AZ_USERNAME}}
 
@@ -12,7 +12,7 @@ rm -rf code/.git
 git fetch --unshallow
 
 # Pull changes from Azure DevOps if its exiting branch and have commits on it
-git pull "https://$AZ_USERNAME:$AZ_PAT@dev.azure.com/$AZ_ORG/code.git"
+git pull "https://$AZ_USERNAME:$AZ_AZURE_CLIENT_SECRET@dev.azure.com/$AZ_ORG/code.git"
 
 #git checkout -b $github_to_azure_sync
 
@@ -23,4 +23,4 @@ git config --global user.name $AZ_USERNAME
 # Add all changes into stage, commit, and push to Azure DevOps
 git add .
 git commit -m "Sync from GitHub to Azure DevOps"
-git push --force "https://$AZ_USERNAME:$AZ_PAT@dev.azure.com/$AZ_ORG/code.git"
+git push --force "https://$AZ_USERNAME:$AZ_AZURE_CLIENT_SECRET@dev.azure.com/$AZ_ORG/code.git"
