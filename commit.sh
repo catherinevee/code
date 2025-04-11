fa95c1eb7437dfa44c9f9ecfc29b0eb84a5fd42e
Catherine Vee <catherine.vee@outlook.com>
2025-04-11 15:04:36 -0700

    remove hard-coded code in commit.sh

--- commit.sh
+++ commit.sh
@@ -7,21 +7,22 @@ AZ_ORG=$AZ_ORG
 AZ_USERNAME=$AZ_USERNAME
 
 AZ_URL="catherinevee/code/_git/code"
+AZ_EMAIL="catherine.vee@outlook.com"
 
 # Fully remove the .git directory
 rm -rf code/.git
 
-# Fetch the changes from Azure DevOps to ensure we have latest
+# Fetch the changes from Azure DevOps repo
 git fetch --unshallow
 
-# Pull changes from Azure DevOps if its exiting branch and have commits on it
-git pull https://catherinevee:$AZ_PAT@dev.azure.com/$AZ_URL
+# Pull changes from Azure DevOps repo
+git pull https://$AZ_USERNAME:$AZ_PAT@dev.azure.com/$AZ_URL
 
-# Set Git user identity
-git config --global user.email "catherine.vee@outlook.com"
+# Set Git Global settings
+git config --global user.email $AZ_EMAIL
 git config --global user.name $AZ_USERNAME
 
 # Add all changes into stage, commit, and push to Azure DevOps
 git add .
 git commit -m "Sync from GitHub to Azure DevOps"
-git push --force https://catherinevee:$AZ_PAT@dev.azure.com/catherinevee/code/_git/code
+git push --force https://$AZ_USERNAME:$AZ_PAT@dev.azure.com/$AZ_URL
