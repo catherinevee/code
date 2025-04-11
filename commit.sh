68447d1a6a6ccae6546e224df41ca40155c84c8e
Catherine Vee <catherine.vee@outlook.com>
2025-04-11 11:12:33 -0700

    modify commit.sh

--- commit.sh
+++ commit.sh
@@ -12,7 +12,7 @@ rm -rf code/.git
 git fetch --unshallow
 
 # Pull changes from Azure DevOps if its exiting branch and have commits on it
-git pull "https://$AZ_USERNAME:$AZ_AZURE_CLIENT_SECRET@dev.azure.com/$AZ_ORG/code.git"
+git pull https://$AZ_USERNAME:$AZ_AZURE_CLIENT_SECRET@dev.azure.com/$AZ_ORG/code.git
 
 #git checkout -b $github_to_azure_sync
 
@@ -23,4 +23,4 @@ git config --global user.name $AZ_USERNAME
 # Add all changes into stage, commit, and push to Azure DevOps
 git add .
 git commit -m "Sync from GitHub to Azure DevOps"
-git push --force "https://$AZ_USERNAME:$AZ_AZURE_CLIENT_SECRET@dev.azure.com/$AZ_ORG/code.git"
+git push --force https://$AZ_USERNAME:$AZ_AZURE_CLIENT_SECRET@dev.azure.com/$AZ_ORG/code.git
