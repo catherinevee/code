daa6f3d5b1572d9162dbbbbcdfd14422f1b5b4fd
Catherine Vee <catherine.vee@outlook.com>
2025-04-11 14:58:55 -0700

    redefine logic of commit.sh

--- commit.sh
+++ commit.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+# Define variables passed in from the pipeline call in GitHub Actions
 AZ_USERNAME=$AZ_USERNAME
 AZ_PAT=$AZ_PAT
 AZ_ORG=$AZ_ORG
 AZ_USERNAME=$AZ_USERNAME
 
-# Remove Git information (for fresh git start)
+AZ_URL="catherinevee/code/_git/code"
+
+# Fully remove the .git directory
 rm -rf code/.git
 
 # Fetch the changes from Azure DevOps to ensure we have latest
 git fetch --unshallow
-echo ">>>> https://$AZ_USERNAME:$AZ_PAT@dev.azure.com/catherinevee/code/_git/code"
-# Pull changes from Azure DevOps if its exiting branch and have commits on it
-git pull https://catherinevee:$AZ_PAT@dev.azure.com/catherinevee/code/_git/code
 
-#git checkout -b $github_to_azure_sync
+# Pull changes from Azure DevOps if its exiting branch and have commits on it
+git pull https://catherinevee:$AZ_PAT@dev.azure.com/$AZ_URL
 
 # Set Git user identity
 git config --global user.email "catherine.vee@outlook.com"
