e8d814af51de08454bd177269125644915f006db
Catherine Vee <catherine.vee@outlook.com>
2025-04-14 16:15:03 -0700

    add man_id to backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,5 +4,8 @@ terraform {
       storage_account_name = "${var.defaultsa}"
       container_name       = "${var.defaultsa-container}"
       key                  = "${var.ARM_ACCESS_KEY}"
+      use_msi = true
+      client_id = "catherinevee_manid"
+
   }
 }
