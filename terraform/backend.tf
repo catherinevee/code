8b5376e650bdd7d6d5a489d36062cee49f1d0958
Catherine Vee <catherine.vee@outlook.com>
2025-04-14 20:08:01 -0700

    update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,6 +6,6 @@ terraform {
       key = "tf/terraform.tfstate"
       use_msi = true
       #client_id = "catherinevee_manid"
-      access_key = "${{secrets.AZURE_SA_TFSTATE_KEY}}"
+      access_key = "${secrets.AZURE_SA_TFSTATE_KEY}"
   }
 }
