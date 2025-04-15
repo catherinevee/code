81f2281ed3d4dd3d3a7762c9b8be207848a5c440
Catherine Vee <catherine.vee@outlook.com>
2025-04-14 20:03:03 -0700

    modify backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,7 @@ terraform {
       resource_group_name  = "${var.tfstateregion}"
       storage_account_name = "${var.tfstatesa}"
       container_name       = "${var.tfstatecontainer}"
+      key = "tf/terraform.tfstate"
       use_msi = true
       client_id = "catherinevee_manid"
 
