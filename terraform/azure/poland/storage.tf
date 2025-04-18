33b2b2d2eef38ccb5e6c26a8e276b8b6c195a619
Catherine Vee <catherine.vee@outlook.com>
2025-04-18 09:19:22 -0700

    add bash task to azure-pipelines-planapply

--- terraform/azure/poland/storage.tf
+++ terraform/azure/poland/storage.tf
@@ -2,7 +2,7 @@ module "avm-res-storage-storageaccount" {
   source  = "Azure/avm-res-storage-storageaccount/azurerm"
   version = "0.5.0"
   location = var.defaultlocation
-  name = "${var.defaultlocation}saprod"
+  name = "polandcentralrg-1"
   resource_group_name = var.defaultrg
   access_tier = "Cool"
   account_kind = "StorageV2"
