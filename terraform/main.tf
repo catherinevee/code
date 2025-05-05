e1fe3afc4d8c5044ce84ccd2e482463b30fdfb54
Catherine Vee <catherine.vee@outlook.com>
2025-05-05 06:41:51 -0700

    move required tf providers from main.tf to versions.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,3 @@
-terraform {
-  #cloud {
-  #  organization = "cathyvee-org"
-  #  workspaces {
-  #    name = "cathyvee"
-  #  }
-    
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.80.0"
-   }
-    google = {
-      source = "hashicorp/google"
-      version = "6.13.0"
-    }
-    azuredevops = {
-      source  = "microsoft/azuredevops"
-      version = ">=0.1.0"
-    } 
-  }
-
-}
-
-
-
 # Configure the AWS Provider
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
---
+++ terraform/versions.tf
@@ -0,0 +1,26 @@
+terraform {
+  #cloud {
+  #  organization = "cathyvee-org"
+  #  workspaces {
+  #    name = "cathyvee"
+  #  }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 3.107.0, < 4.0"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.80.0"
+   }
+    google = {
+      source = "hashicorp/google"
+      version = "6.13.0"
+    }
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">=0.1.0"
+    } 
+  }
+
+}
