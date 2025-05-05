005031df1a09cca1eb7824d002c1c8043b1b22da
Catherine Vee <catherine.vee@outlook.com>
2025-05-05 16:52:45 -0700

    edit outputs.tf, priv-loadbalancer.tf

--- terraform/azure/poland/outputs.tf
+++ terraform/azure/poland/outputs.tf
@@ -2,7 +2,7 @@
 output "tfstate_storage_account_id" {
   value = "${data.azurerm_storage_account.tfstate_sa.id}"
 }
-
+#required for vnet peering
 data "azurerm_virtual_network" "data_defaultvnet_id" {
  depends_on = [
     module.avm-res-network-virtualnetwork-vnet1
@@ -10,12 +10,10 @@ data "azurerm_virtual_network" "data_defaultvnet_id" {
   name                = join("_",[var.defaultlocation, var.defaultenv, "vnet1"])
   resource_group_name = var.defaultrg
 }
-
 output "virtual_network1_id" {
   value = data.azurerm_virtual_network.data_defaultvnet_id.id
 }
-
-
+#required for vnet peering
 data "azurerm_virtual_network" "data_secondvnet_id" {
  depends_on = [
     module.avm-res-network-virtualnetwork-vnet2
@@ -23,19 +21,29 @@ data "azurerm_virtual_network" "data_secondvnet_id" {
   name                = join("_",[var.defaultlocation, var.defaultenv, "vnet2"])
   resource_group_name = var.defaultrg
 }
-
 output "virtual_network2_id" {
   value = data.azurerm_virtual_network.data_secondvnet_id
 }
 
-
+#kubernetes
 output "client_certificate" {
   value     = azurerm_kubernetes_cluster.poland-aks.kube_config[0].client_certificate
   sensitive = true
 }
-
 output "kube_config" {
   value = azurerm_kubernetes_cluster.poland-aks.kube_config_raw
 
   sensitive = true
 }
+
+#
+
+data "azurerm_subnet" "subnet1" {
+    name                 = module.avm-res-network-virtualnetwork-vnet1.name
+    virtual_network_name = "${var.defaultenv}subnet1"
+    resource_group_name  =  "${var.defaultrg}"
+}
+
+ output "subnet_id" {
+     value = "${data.azurerm_subnet.subnet1.id}"
+ }
--- terraform/azure/poland/priv-loadbalancer.tf
+++ terraform/azure/poland/priv-loadbalancer.tf
@@ -2,7 +2,7 @@ module "mylb" {
   source                                 = "Azure/loadbalancer/azurerm"
   resource_group_name                    = var.defaultrg
   type                                   = "private"
-  frontend_subnet_id                     = "sdfsdfs"
+  frontend_subnet_id                     = data.azurerm_subnet.subnet1
   frontend_private_ip_address_allocation = "Static"
   frontend_private_ip_address            = "10.1.2.254"
   lb_sku                                 = "Standard"
