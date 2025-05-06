5e962a39531b848d1d758b8e3ad5c51f057d0a26
Catherine Vee <catherine.vee@outlook.com>
2025-05-06 16:52:55 -0700

    remove unused modules from main.tf in apsoutheast-1

--- terraform/aws/ap-southeast-1/main.tf
+++ terraform/aws/ap-southeast-1/main.tf
@@ -2,15 +2,3 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
-
-module "s3-bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.6.0"
-}
-
-
-module "rds" {
-  source  = "terraform-aws-modules/rds/aws"
-  version = "6.11.0"
-  identifier = "demodb"
-}
