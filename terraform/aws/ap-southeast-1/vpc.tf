29076c7fee68180d48eaef3300d9f0a939f76304
Catherine Vee <catherine.vee@outlook.com>
2025-05-06 11:22:31 -0700

    modify vpc.tf for apsoutheast1

--- terraform/aws/ap-southeast-1/vpc.tf
+++ terraform/aws/ap-southeast-1/vpc.tf
@@ -1,3 +1,9 @@
+#these are used to re-use elastic ips instead of creating new ones 
+resource "aws_eip" "nat" {
+  count = 3
+
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -8,8 +14,11 @@ module "vpc" {
   private_subnets = var.default_privatesubnets
   public_subnets  = var.default_publicsubnets
 
-  enable_nat_gateway = true
+  enable_nat_gateway = true #per subnet
+  #single_nat_gateway = true
+  #one-nat-gateway-per-az = true
   enable_vpn_gateway = true
-
+  reuse_nat_ips       = true         
+  external_nat_ip_ids = "${aws_eip.nat.*.id}"
   tags = var.defaulttags
 }
