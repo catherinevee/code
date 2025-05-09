661395ae3956bc52b32adc0e9fb099a7c38a462c
Catherine Vee <catherine.vee@outlook.com>
2025-05-08 18:05:12 -0700

    remove env variable from azure-pipelines-planapply

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
 # Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1" #default region, overridden by variable 
-}
+provider "aws" {}#default region, overridden by variable 
+
 provider "azurerm" {
   features {}  
   use_msi = true
