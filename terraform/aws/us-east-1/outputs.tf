8fd29e1616f9fdb26a9f615daaf5b64267e94eed
Catherine Vee <catherine.vee@outlook.com>
2025-05-13 13:26:30 -0700

    modify aws_subnets resource in outputs.tf

--- terraform/aws/us-east-1/outputs.tf
+++ terraform/aws/us-east-1/outputs.tf
@@ -9,7 +9,7 @@ output "output_subnet_ids" {
 data "aws_subnets" "defaultvpc" {
   filter {
     name   = "vpc-id"
-    values = [output.output_defaultvpc_id]
+    values = [module.vpc.default_vpc_id]
   }
 }
 
