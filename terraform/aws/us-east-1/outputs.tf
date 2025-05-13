c9f1f2346f2aba34a38d24fdc4db0ee068f457e0
Catherine Vee <catherine.vee@outlook.com>
2025-05-13 13:14:40 -0700

    add aws_subnets data resources to outputs.tf in useast1

--- terraform/aws/us-east-1/outputs.tf
+++ terraform/aws/us-east-1/outputs.tf
@@ -5,3 +5,19 @@ output "output_defaultvpc_id" {
 output "output_subnet_ids" {
     value = module.vpc.private_subnets.*
 }
+
+data "aws_subnets" "defaultvpc" {
+  filter {
+    name   = "vpc-id"
+    values = [output_defaultvpc_id]
+  }
+}
+
+data "aws_subnet" "defaultsubnets" {
+  for_each = toset(data.aws_subnets.defaultvpc.id)
+  id       = each.value
+}
+
+output "subnet_cidr_blocks" {
+  value = [for s in data.aws_subnet.defaultsubnets : s.ids]
+}
