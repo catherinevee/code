523ed32aca9bfe2e9ded3005aff682a6f136d007
Catherine Vee <catherine.vee@outlook.com>
2025-05-19 17:52:09 -0700

    edit vnet.tf for mexicocentral

--- terraform/azure/mexicocentral/vnet.tf
+++ terraform/azure/mexicocentral/vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "mexicocentralvnet" {
   name                = "vnet_${var.defaultlocation}"
-  location            = azurerm_resource_group.polandcentral-prod-resourcegroups[2].location
-  resource_group_name = azurerm_resource_group.polandcentral-prod-resourcegroups[2].location
+  location            = azurerm_resource_group.mexicocentralresourcegroups[2].location
+  resource_group_name = azurerm_resource_group.mexicocentralresourcegroups[2].location
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
