ced5776a34da1b4536a9043d88263f3a300da11e
Catherine Vee <catherine.vee@outlook.com>
2025-05-19 17:57:21 -0700

    fix variables in vnet.tf, rg.tf

--- terraform/azure/mexicocentral/rg.tf
+++ terraform/azure/mexicocentral/rg.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "polandcentral-prod-resourcegroups" {
+resource "azurerm_resource_group" "mexicocentralresourcegroups" {
   location = var.defaultlocation
   for_each = toset(var.mexicocentralresourcegroups)
   name = each.value  
