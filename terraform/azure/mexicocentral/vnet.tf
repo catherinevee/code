04f58491ef9b159932a665127081dfb3df8bfb6b
Catherine Vee <catherine.vee@outlook.com>
2025-05-19 17:47:55 -0700

    fix resource in vnet.tf

--- terraform/azure/mexicocentral/vnet.tf
+++ terraform/azure/mexicocentral/vnet.tf
@@ -5,16 +5,5 @@ resource "azurerm_virtual_network" "mexicocentralvnet" {
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  subnet {
-    name             = "1_${azurerm_resource_group.polandcentral-prod-resourcegroups[2].location}_subnet"
-    address_prefixes = ["10.0.1.0/24"]
-  }
-
-  subnet {
-    name             = "2_${azurerm_resource_group.polandcentral-prod-resourcegroups[2].location}_subnet"
-    address_prefixes = ["10.0.2.0/24"]
-    #security_group   = azurerm_network_security_group.sg.id
-  }
-
   tags = var.tags
 }
