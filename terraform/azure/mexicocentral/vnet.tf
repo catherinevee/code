ef4d14e914b92e3ad34fbe10560294ece04cfb65
Catherine Vee <catherine.vee@outlook.com>
2025-05-19 18:02:50 -0700

    add reference in vnet.tf for mexicocentral

--- terraform/azure/mexicocentral/vnet.tf
+++ terraform/azure/mexicocentral/vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "mexicocentralvnet" {
   name                = "vnet_${var.defaultlocation}"
-  location            = var.mexicocentralresourcegroups[2].location
-  resource_group_name = var.mexicocentralresourcegroups[2].location
+  location            = var.mexicocentralresourcegroups[2]
+  resource_group_name = var.mexicocentralresourcegroups[2]
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
