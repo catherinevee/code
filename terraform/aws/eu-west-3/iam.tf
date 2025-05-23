b0ccbc81c60fee46160c2a17503bd8d0dc6f7376
Catherine Vee <catherine.vee@outlook.com>
2025-05-23 13:04:11 -0700

    upload iam.tf, lambda.tf to aws paris

---
+++ terraform/aws/eu-west-3/iam.tf
@@ -0,0 +1,36 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "tflambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy" "lambda" {
+  name = "lambda-permissions"
+  role = aws_iam_role.lambda_role.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
---
+++ terraform/aws/eu-west-3/lambda.tf
@@ -0,0 +1,29 @@
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/index.js"
+  output_path = "${path.module}/lambda/lambda.zip"
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = data.archive_file.lambda.output_path
+  function_name = "lambdafunction"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "nodejs18.x"
+  timeout = 15
+  memory_size = 1024
+  environment {
+    variables = {
+      PRODUCTION = false
+    }
+}
+}
+
+resource "aws_lambda_function_url" "lambda" {
+  function_name      = aws_lambda_function.lambda.function_name
+  authorization_type = "NONE"
+}
