fa5da2d27f0f99334dc22411a7badc888e9d3e4d
Catherine Vee <catherine.vee@outlook.com>
2025-05-22 20:06:30 -0700

    change value in outputs.tf

--- terraform/azure/mexicocentral/outputs.tf
+++ terraform/azure/mexicocentral/outputs.tf
@@ -1,17 +1,1 @@
-
-output "express_route_circuit_id" {
-  value = azurerm_express_route_circuit.expressroute1_circuit.id
-}
-
-output "gateway_name" {
-  value = azurerm_virtual_network_gateway.expressroute1_ergateway.name
-}
-
-output "gateway_ip" {
-  value = azurerm_public_ip.gateway_ip.ip_address
-}
-
-output "service_key" {
-  value     = azurerm_express_route_circuit.expressroute1_circuit.service_key
-  sensitive = true
-}
+#
